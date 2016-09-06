f398aeb0984985830df9348e63e3a40cf66f7970
Nolan Morache <nolan241@gmail.com>
2016-09-06 01:57:41 +0000

    Add users to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905222809) do
+ActiveRecord::Schema.define(version: 20160906014055) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
