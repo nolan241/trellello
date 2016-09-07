2dc7cf7840c625e95aa5a6d4368b886cda16454f
Nolan Morache <nolan241@gmail.com>
2016-09-07 03:42:06 +0000

    Add Change functionality for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :change]
   before_action :authenticate_user!
 
   # GET /tasks
@@ -62,6 +62,13 @@ class TasksController < ApplicationController
     respond_to do |format|
       format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
+    end
+  end
+  
+  def change
+    @task.update_attributes(state: params[:state])
+    respond_to do |format| 
+      format.html { redirect_to tasks_path, notice: "Task status successfully changed"}
     end
   end
 
